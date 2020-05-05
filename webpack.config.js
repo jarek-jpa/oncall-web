6fda9353a9fb469e725ec3847ab08873719e7548
Jarek Pawlak <jaroslaw.pawlak@ocado.com>
2020-05-05 21:18:21 +0200

    tuning TS and webpack a bit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const path = require("path");
-
 module.exports = {
     // Set debugging source maps to be "inline" for
     // simplicity and ease of use
@@ -31,5 +29,14 @@ module.exports = {
     // File extensions to support resolving
     resolve: {
         extensions: [".ts", ".tsx", ".js"]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        react: "React",
+        "react-dom": "ReactDOM"
     }
 };
